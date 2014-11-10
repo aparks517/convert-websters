7da876a239deddfacad2c489c9efa91744581994
Aaron D. Parks <aparks@aftermath.net>
2014-11-10 11:49:55 -0500

    Switched to many small "quick indices" arranged by the first two letters of the words they contain rather than one big word list.

--- convert-web.pl
+++ convert-web.pl
@@ -8,7 +8,6 @@ use strict;
 
 # settings
 my $output_directory = shift || '.';
-my $words_directory = shift || '';
 my $max_words = shift || 0;
 
 # read
@@ -46,21 +45,76 @@ if ($max_words){
 	warn "Elimited all but " . scalar(@word_list) . " words\n";
 }
 
-# word list header and list open tag
-print "<!-- \@include _wordlist_header -->\n<ul id='wordlist'>\n";
+# produce quick index, which is a hash with the first two letters of words
+# as keys and arrayrefs of words as values
+my %quick_index;
+for my $word (@word_list){
+	# first two letters of word
+	my $first_two = substr($word, 0, 2);
+
+	# if there is already a quick index key for the first two letters of this
+	# word, add the word to that arrayref.  otherwise, add the key.
+	if (defined($quick_index{$first_two})){
+		push @{$quick_index{$first_two}}, $word;
+	} else {
+		$quick_index{$first_two} = [$word];
+	}
+}
+
+# index directory; create if doesn't exist
+my $index_directory = "$output_directory/indices";
+unless (-d $index_directory){
+	mkdir $index_directory || die "couldn't create index directory";
+	warn "Created index directory $index_directory\n";
+}
+
+# open meta index file (index of quick indices) and list tag
+open META, ">$index_directory/meta.html" || die "couldn't open meta index file";
+print META "<!-- \@include _index_header -->\n<ul>\n";
+
+# emit quick index files
+for my $first_two (sort(keys(%quick_index))){
+	# meta index link
+	my $uri = uri_escape("$first_two.html");
+	my $encoded_first_two = encode_entities($first_two);
+	my $word_count = scalar(@{$quick_index{$first_two}});
+	print META "<li>$word_count words beginning with <a href='$uri'>$encoded_first_two</a></li>\n";
+
+	# open file and open word list tag
+	open INDEX, ">$index_directory/$first_two.html" || die "couldn't open quick index file";
+	print INDEX "<!-- \@include _index_header -->\n<ul id='word-list'>\n";
+
+	# words
+	for my $word (@{$quick_index{$first_two}}){
+		my $uri = "../words/" . uri_escape("$word.html");
+		my $encoded_word = encode_entities($word);
+		print INDEX "<li><a href='$uri'>$encoded_word</a></li>\n";
+	}
+
+	# close word list tag and file
+	print INDEX "</ul>\n<!-- \@include _index_footer -->\n";
+	close INDEX;
+}
+
+# close meta index list tag and file
+print META "</ul>\n<!-- \@include _index_footer -->\n";
+close META;
+
+# words directory; create if doesn't exist
+my $words_directory = "$output_directory/words";
+unless (-d $words_directory){
+	mkdir $words_directory || die "couldn't create words directory";
+	warn "Created words directory $words_directory\n";
+}
 
 # iterate word list, emitting a file in the output directory for each word 
 # and word list links on stdout
 for my $word (@word_list){
-	# word list entry
-	my $uri = $words_directory . "/" .  uri_escape("$word.html");
-	my $encoded_word = encode_entities($word);
-	print "<li><a href='$uri'>$encoded_word</a></li>\n";
-
 	# open word file
-	open WORD, ">$output_directory/$word.html" || die "couldn't open word file";
+	open WORD, ">$words_directory/$word.html" || die "couldn't open word file";
 
 	# word header
+	my $encoded_word = encode_entities($word);
 	print WORD "<!-- \$title $encoded_word -->\n<!-- \@include _word_header -->\n";
 
 	# iterate entries
@@ -77,7 +131,7 @@ for my $word (@word_list){
 
   		# subhead
 		my $subhead;
-		for my $key (qw(pronunciation part_of_speech etymology specialty)){
+		for my $key (qw(pronunciation part-of-speech etymology specialty)){
 			my $value = $entry->{$key};
 			if (defined($value)){
 				$value = encode_entities($value);
@@ -102,6 +156,3 @@ for my $word (@word_list){
 	# close word file
 	close WORD;
 } # iterate words
-
-# list close tag and word list footer
-print "</ul>\n<!-- \@include _wordlist_footer -->\n";
